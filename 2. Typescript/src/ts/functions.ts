87904b591decb0a3e5de2fe70592354d9317cdab
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-01-12 18:43:39 +0100

    Added demos

---
+++ 2. Typescript/src/ts/functions.ts
@@ -0,0 +1,10 @@
+import { Car } from "./models/car";
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export const addCar = (brand: string, model: string, cars: Car[]) => {
+  let newCar = new Car(brand, model);
+
+  cars.push(newCar);
+};
